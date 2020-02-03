ec21e6f41d5d2d5a97abc8583552547be3ad12e5
Keturah Howard and Michelle Morin <michelle@>
2020-02-03 09:57:51 -0800

    add initial business logic for journal

---
+++ src/journal.js
@@ -0,0 +1,20 @@
+export function Entry() {
+  this.title = "",
+  this.body = "",
+  this.words = [],
+  this.numberOfWords = 0
+}
+
+Entry.prototype.addTitle = function(title) {
+  this.title = title;
+}
+
+Entry.prototype.addBody = function(body) {
+  this.body = body;
+}
+
+Entry.prototype.countWords = function() {
+  this.words = this.body.split(" ");
+  this.numberOfWords = this.words.length;
+}
+
