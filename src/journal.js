dc4f49c0196543ea04a4a93fa98e705198cbf290
Keturah Howard and Michelle Morin <michelle@>
2020-02-03 11:09:46 -0800

    add new prototype to count vowels

--- src/journal.js
+++ src/journal.js
@@ -1,20 +1,35 @@
 export function Entry() {
-  this.title = "",
-  this.body = "",
-  this.words = [],
-  this.numberOfWords = 0
+  this.title = "";
+  this.body = "";
+  this.words = [];
+  this.characters = [];
+  this.numberOfWords = 0;
+  this.numberOfVowels = 0;
+  this.vowels = [];
+  this.consonates = [];
 }
 
 Entry.prototype.addTitle = function(title) {
   this.title = title;
-}
+};
 
 Entry.prototype.addBody = function(body) {
   this.body = body;
-}
+};
 
 Entry.prototype.countWords = function() {
   this.words = this.body.split(" ");
   this.numberOfWords += this.words.length;
-}
+};
 
+Entry.prototype.countVowels = function(){
+  this.characters = this.body.split("");
+
+  for (var i = 0; i < this.characters.length; i ++ ) {
+    var vowels = ['a', 'e', 'i', 'o', 'u']
+    if (vowels.includes(this.characters[i])) {
+      this.vowels.push(this.characters[i]);
+    }
+  }
+  this.numberOfVowels += this.vowels.length;
+}
